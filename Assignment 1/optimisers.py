078d5511c14fff1197f1f2e34c28c120b92837aa
go4rav <36837109+go4rav@users.noreply.github.com>
2022-02-18 23:52:18 +0530

    Added optimsers
    momentum, rms, Adam

---
+++ Assignment 1/optimisers.py
@@ -0,0 +1,35 @@
+
+
+
+def updateWeightsMomentum(params,gradients,layers,alpha,beta,M):
+    for i in range(1,len(layers)):
+        M["W"+str(i)]=beta*V["W"+str(i)]+(1-beta)*gradients["dw"+str(i)]
+        M["b"+str(i)]=beta*V["b"+str(i)]+(1-beta)*gradients["db"+str(i)]
+        params["W"+str(i)]-= alpha* M["W"+str(i)]
+        params["b"+str(i)]-= alpha*M["b"+str(i)]
+    return(params)
+
+def updateWeightsAdam(params,gradients,layers,alpha,gamma1,gamma2,eps,t,M,R):
+    M_c={}
+    R_c={}
+    for i in range(1,len(layers)):
+        M["W"+str(i)]=gamma1*M["W"+str(i)]+(1-gamma1)*gradients["dw"+str(i)]
+        M["b"+str(i)]=gamma1*M["b"+str(i)]+(1-gamma1)*gradients["db"+str(i)]
+        M_c["W"+str(i)]=M["W"+str(i)]/(1-np.power(gamma1,t))
+        M_c["b"+str(i)]=M["b"+str(i)]/(1-np.power(gamma1,t))
+        R["W"+str(i)]=gamma2*R["W"+str(i)]+(1-gamma2)*np.power(gradients["dw"+str(i)],2)
+        R["b"+str(i)]=gamma2*R["b"+str(i)]+(1-gamma2)*np.power(gradients["db"+str(i)],2)
+        R_c["W"+str(i)]=R["W"+str(i)]/(1-np.power(gamma2,t))
+        R_c["b"+str(i)]=R["b"+str(i)]/(1-np.power(gamma2,t))
+        params["W"+str(i)]-= (alpha*M_c["W"+str(i)])/(np.sqrt(R_c["W"+str(i)])+eps)
+        params["b"+str(i)]-= (alpha*M_c["b"+str(i)])/(np.sqrt(R_c["b"+str(i)])+eps)
+    return(params)
+
+
+def updateWeightsRMS(params,gradients,layers,alpha,beta,R):
+    for i in range(1,len(layers)):
+        R["W"+str(i)]=beta*R["W"+str(i)]+(1-beta)*np.power(gradients["dw"+str(i)],2)
+        R["b"+str(i)]=beta*R["b"+str(i)]+(1-beta)*np.power(gradients["db"+str(i)],2)
+        params["W"+str(i)]-= alpha* R["W"+str(i)]
+        params["b"+str(i)]-= alpha* R["b"+str(i)]
+    return(params)
