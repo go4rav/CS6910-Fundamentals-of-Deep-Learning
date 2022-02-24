777e04809f1f3c7b46c9ff1e17e79d5e549a45fd
go4rav <36837109+go4rav@users.noreply.github.com>
2022-02-24 13:49:33 +0530

    Update main.py

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 import matplotlib.pyplot as plt
-from optimisers.py import updateWeightsMomentum, updateWeightsAdam, updateWeightsRMS
+from optimisers.py import updateWeightsSGD, updateWeightsMomentum, updateWeightsAdam, updateWeightsRMS, updateWeightsNAdam, updateWeightsAdam
 
 
 
@@ -34,6 +34,9 @@ def OneHotEncoding(Y_train,num_train):
     index=Y[i,0]
     Y_train[index,i]=1
   return(Y_train, Y)
+
+
+
 
 
 def shuffle_data(X_train, Y_train):
@@ -69,7 +72,7 @@ def get_mini_batches(X_train, Y_train,mini_batch_size):
       Y_mini_batches.append(y)
     
     
-    return(X_mini_batches, Y_mini_batches)     
+    return(X_mini_batches, Y_mini_batches)      
 
 
 # Activations and their gradients
@@ -115,12 +118,15 @@ def softmax(z):
 
 
 
-
-def initialize_weights(layers, params, M, R):
+def initialize_weights(layers, params, M, R, initialiser):
   # seed=3
   # np.random.seed(seed)
   for i in range(1,len(layers)):
-      params["W"+str(i)]=np.random.randn(layers[i],layers[i-1])*np.sqrt(2 / layers[i-1])
+      if(initialiser=="xavier"):
+        params["W"+str(i)]=np.random.randn(layers[i],layers[i-1])*np.sqrt(2 / (layers[i]+layers[i-1]))
+      elif(initialiser=="random"):
+        params["W"+str(i)]=np.random.randn(layers[i],layers[i-1])*0.01
+
       params["b"+str(i)]=np.zeros([layers[i],1])
       M["W"+str(i)]=np.zeros([layers[i],layers[i-1]])
       M["b"+str(i)]=np.zeros([layers[i],1])
@@ -129,7 +135,7 @@ def initialize_weights(layers, params, M, R):
   return(params, M, R)
 
 
-def feedforward(params,layers,X):
+def feedforward(params,layers,X,activation):
     
     a=X
     n=len(layers)-1
@@ -141,7 +147,12 @@ def feedforward(params,layers,X):
         W=params["W"+str(i)]
         b=params["b"+str(i)]
         z = np.dot(W,a)+b
-        a = relu(z)
+        if(activation=="relu"):
+          a = relu(z)
+        elif(activation=="sigmoid"):
+          a = sigmoid(z)
+        elif(activation=="tanh"):
+          a = tanh(z)
         A.append(a)
         Z.append(z)
     W=params["W"+str(n)]
@@ -154,7 +165,7 @@ def feedforward(params,layers,X):
 
 
 
-def backwardPropagate(params, layers,Z,A,alpha,Y):
+def backwardPropagate(params, layers, Z, A, learning_rate, Y, activation):
     m=Y.shape[1]
     l=len(layers)-1
     dz= A[l]-Y
@@ -167,26 +178,53 @@ def backwardPropagate(params, layers,Z,A,alpha,Y):
         gradients["db"+str(l)]=db
         if(l>=2):
             da= np.dot(np.transpose(params["W"+str(l)]), dz)
-            dz = da*reluGradient(Z[l-1])
+            if(activation=="relu"):
+              dz = da*reluGradient(Z[l-1])
+            elif(activation=="sigmoid"):
+              dz = da*sigmoidGradient(Z[l-1])
+            elif(activation=="tanh"):
+              dz = da*tanhGradient(Z[l-1])
         l=l-1
     return(gradients)
+
         
-def crossEntropyError(a,Y):
+def crossEntropyError(a,Y,params,layers,weight_decay):
     m=a.shape[1]
     error=-(np.sum(np.sum(Y*np.log(a),axis=1),axis=0))
     #error=-np.sum(Y*np.log(a)+(1-Y)*np.log(1-a))/m
+    for i in range(1,len(layers)):
+      regu_cost = (weight_decay/2)*np.sum(np.sum(np.square(params["W"+str(i)]),axis=1),axis=0)
+    error+=regu_cost
     return(error)
      
-def squaredError(a,Y):
+def squaredError(a,Y,params,layers,weight_decay):
     m=a.shape[1]
-    error=np.sum(np.square(a-Y),axis=1)/m
+    error=np.sum(np.square(a-Y),axis=1)
+    for i in range(1,len(layers)):
+      regu_cost = (weight_decay/2)*np.sum(np.sum(np.square(params["W"+str(i)]),axis=1),axis=0)
+    error+=regu_cost
     return(error)
 
 
-def predict(params, layers, X_train, X_test, Y_train_orig, Y_test):
+
+
+def predict(params, layers, X_train, X_test, Y_train_orig, Y_test,activation):
   num_train = X_train.shape[1]
   num_test = X_test.shape[1]
-  A,Z=feedforward(params, layers, X_test)
+  
+
+  A,Z=feedforward(params, layers, X_train,activation)
+  pred=A[-1]
+  max_index = np.argmax(pred, axis=0)
+  count=0
+  for i in range(num_train):
+      if(Y_train_orig[0,i]==max_index[i]):
+          count+=1
+  
+  print("train accuracy: ",(count/num_train)*100)
+  validationaccuracy = (count/num_train)*100
+
+  A,Z=feedforward(params, layers, X_test,activation)
   pred=A[-1]
   max_index = np.argmax(pred, axis=0)
   count=0
@@ -195,19 +233,10 @@ def predict(params, layers, X_train, X_test, Y_train_orig, Y_test):
           count+=1
   print("test accuracy: ",(count/num_test)*100)
 
-  A,Z=feedforward(params, layers, X_train)
-  pred=A[-1]
-  max_index = np.argmax(pred, axis=0)
-  count=0
-  for i in range(num_train):
-      if(Y_train_orig[0,i]==max_index[i]):
-          count+=1
-  print("train accuracy: ",(count/num_train)*100)
   return((count/num_train)*100)
           
       
   
-
 (X_train, Y_train), (X_test, Y_test) = data.load_data()
 (X_train, X_test) = flatten_input(X_train,X_test)
 
@@ -225,108 +254,78 @@ Y_test= Y_test.reshape(1,num_test)
 print(X_train.shape,Y_train.shape, X_test.shape, Y_test.shape)
 
 
-
-
-sweep_config = {
-  "name": "Bayesian Sweep",
-  "method": "bayes",
-  "metric":{
-  "name": "validationaccuracy",
-  "goal": "maximize"
-  },
-  "parameters": {
-        "iters": {
-            "values": [5, 10]
-        },
-
-        "initializer": {
-            "values": ["RANDOM", "XAVIER", "HE"]
-        },
-
-        "layers": {
-            "values": [2, 3, 4]
-        },
-        
-        
-        "num_hidden_neurons": {
-            "values": [32, 64, 128]
-        },
-        
-        "activation": {
-            "values": [ 'SIGMOID', 'RELU']
-        },
-        
-        "learning_rate": {
-            "values": [0.001, 0.0001]
-        },
-        
-        
-        "weight_decay": {
-            "values": [0, 0.0005,0.5]
-        },
-        
-        "optimizer": {
-            "values": ["SGD", "MGD", "RMSPROP", "ADAM"]
-        },
-                    
-        "batch_size": {
-            "values": [16,32,64]
-        }
-        
-        
-    }
-}
-
-sweep_id = wandb.sweep(sweep_config,project='Assignment__', entity='go4rav')
-
-
 def train_model(X_train, Y_train,X_test, Y_test, Y_train_orig):
-    wandb.init(project="assignment_1_",entity='go4rav')
-    CONFIG=wandb.config
-    print("Testing",CONFIG)
-    mini_batch_size=CONFIG["batch_size"]
-    layers=[784,32,32,32,10] 
+	
+    mini_batch_size=64
+    epochs=10
+    initialiser = 'xavier'
+    optimiser= 'nadam'
+    weight_decay=0
+    learning_rate=0.001
+    hidden_layers=int(input())
+    size_hidden_layer=int(input())
+    activation= 'relu'	
+	
+    layers=[X_train.shape[0]]
+    for i in range(hidden_layers):
+      layers.append(size_hidden_layer)
+    layers.append(Y_train.shape[0])
     params={}
     M={}
     R={}
+    lookahead_params={}
     errors=[]
-    params, M, R = initialize_weights(layers, params, M, R)
     
-
+    params, M, R = initialize_weights(layers, params, M, R,initialiser)
     (X_mini_batches, Y_mini_batches) = get_mini_batches(X_train, Y_train,mini_batch_size)
-
-
     errors=[]
-    iters= 1
+    epoch=1
     gamma1=0.9
     gamma2=0.999
     eps=1e-8
-    alpha=0.001
     seed=10
+    beta=0.9
     m=X_train.shape[1]
     
     t=0
 
-    while(iters<=10):
+    
+    while(epoch<=epochs):
         error=0
         for i in range(len(X_mini_batches)):
           X_train_mini=X_mini_batches[i]
           Y_train_mini=Y_mini_batches[i]
-          A,Z=feedforward(params, layers, X_train_mini)
-          error+= crossEntropyError(A[-1], Y_train_mini)
+          A,Z=feedforward(params, layers, X_train_mini,activation)
+          error+= crossEntropyError(A[-1], Y_train_mini,params,layers,weight_decay)
           #error=squaredError(A[-1],Y_train)
-          gradients=backwardPropagate(params, layers,Z, A, alpha, Y_train_mini)
-          t=t+1
-          updateWeights(params,gradients,layers,alpha,optimiser,M,R,gamma1,gamma2,beta,eps,t,optimiser)
-        error=error/m
-        if(iters<=10):
-          errors.append(error)
-          print(error,iters)
+          if(optimiser=="nadam" or optimiser=="nesterov"):
+              for i in range(1,len(layers)):
+                lookahead_params["W"+str(i)]=params["W"+str(i)]-M["W"+str(i)]
+                lookahead_params["b"+str(i)]=params["b"+str(i)]-M["b"+str(i)]
+              lookahead_grads=backwardPropagate(lookahead_params, layers,Z, A, learning_rate, Y_train_mini,activation)
+              if(optimiser=="nesterov"):
+                params, M = updateWeightsNesterov(num_train, params, lookahead_grads, M, layers, learning_rate, weight_decay, beta)
+              elif(optimiser=="nadam"):
+                t=t+1
+                params,M,R = updateWeightsNAdam(num_train, params, lookahead_grads, layers, M, R, learning_rate, weight_decay, gamma1,gamma2,eps,t)
 
-        elif(iters%10==0):
-          errors.append(error)
-          print(error,iters)
-        iters+=1
+          else:
+              gradients=backwardPropagate(params, layers,Z, A, learning_rate, Y_train_mini, activation)
+              if(optimiser=="sgd"):
+                params = updateWeightsSGD(num_train, params,gradients,layers, learning_rate, weight_decay)
+              elif(optimiser=="momentum"):
+                params,M = updateWeightsMomentum(num_train, params, gradients, M, layers, learning_rate,weight_decay, beta)
+              elif(optimiser=="rmsprop"):
+                params, R = updateWeightsRMS(num_train, params, gradients, R, layers, learning_rate, weight_decay, beta ,eps)
+              elif(optimiser=="adam"):
+                t=t+1
+                params, M, R = updateWeightsAdam(num_train,params, gradients, layers, M, R, learning_rate, weight_decay, gamma1, gamma2, eps, t)
+        error=error/m
+        errors.append(error)
+        print(error,epoch)
+        epoch+=1
+	
+	
       
     #A,Z=feedforward(params, layers, X_train)
     #print(A[-1])
@@ -335,8 +334,14 @@ def train_model(X_train, Y_train,X_test, Y_test, Y_train_orig):
     plt.xlabel("EPOCHS")
     plt.ylabel("Loss value")
 
-    validationaccuracy=predict(params, layers, X_train, X_test, Y_train_orig, Y_test)
+    validationaccuracy=predict(params, layers, X_train, X_test, Y_train_orig, Y_test,activation)
 	
 	
-#train_model(X_train, Y_train,X_test, Y_test, Y_train_orig)
-wandb.agent(sweep_id, train_model,count = 2)
+	
+	
+train_model()
+
+
+
+
+
