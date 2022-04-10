f3071677688bc8a21e21ee8fc376101c7905c165
go4rav <36837109+go4rav@users.noreply.github.com>
2022-04-10 22:29:26 +0530

    Create test_accuracy.py

---
+++ Assignment2/PartA/test_accuracy.py
@@ -0,0 +1,27 @@
+from keras_preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+from keras import layers
+from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
+from keras.models import Model
+from keras.models import Sequential
+
+
+loaded_model = tf.keras.models.load_model("./best_model")
+
+
+#test data generation
+test_datagen = ImageDataGenerator(
+    rescale=1.0 / 255,
+  )
+
+test_set = test_datagen.flow_from_directory(
+      test_dir,
+      target_size=(256, 256),
+      class_mode='categorical',
+      shuffle=True,
+      seed=100,
+  )
+
+# model evaluation on test set
+loaded_model.evaluate(test_set)
